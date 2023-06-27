4857baa0f45822fcac44d7ac36b3f864b24773de
EAnchugov <bergy@yandex.ru>
2023-06-27 16:22:44 +0300

    16/100

--- src/week3/prctice/DiscreteDistribution.java
+++ src/week3/prctice/DiscreteDistribution.java
@@ -1,6 +1,4 @@
-package week3.prctice;
-
-//Write a program DiscreteDistribution.java that takes an integer command-line argument m,
+// Write a program DiscreteDistribution.java that takes an integer command-line argument m,
 //        followed by a sequence of positive integer command-line arguments a1,a2,…,an
 //        , and prints m random indices (separated by whitespace), choosing each index i
 //        with probability proportional to ai
@@ -11,21 +9,21 @@ public class DiscreteDistribution {
         int[] a = new int[(args.length - 1)];
 //        System.out.println(i);
 //        System.out.println(a.length);
-        for (int m = 1; m < args.length; m++){
+        for (int m = 1; m < args.length; m++) {
             a[m-1] = Integer.parseInt(args[m]);
 //            System.out.println( a[m-1]);
         }
 //        for (int k = 0; k < args.length-1; k++){
 //            System.out.println(a[k]);
 //        }
-        int s = 0;
-        for (int k = 0; k < args.length-1; k++){
-            s = s + a[k];
-        }
-        for (int t = 0; t < i-1; t++){
+//        int s = 0;
+//        for (int k = 0; k < args.length-1; k++) {
+//            s = s + a[k];
+//        }
+        for (int t = 0; t < i; t++) {
 //            System.out.println((int)(Math.random() * r) + 1);
-            int r = (int) (Math.random() * a.length - 1);
-            System.out.println(r + 1);
+            int r = (int) (Math.random() * a.length);
+            System.out.print(r + 1 + "  ");
         }
 //        System.out.println(s);
     }
